602d9d411ad88c401c2bb3e57b4716afcab32016
Carson Barry <carsonbarry@macbook-pro.tailadd55.ts.net>
2023-09-27 17:42:59 -0500

    tree fully functional

--- binary-tree.js
+++ binary-tree.js
@@ -41,35 +41,6 @@ class BinaryTree {
 
   }
 
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-  areCousins(node1, node2) {
-
-  }
-
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
-    
-  }
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
--- tree.js
+++ tree.js
@@ -15,20 +15,62 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    const toVisitStack = [this.root];
+    let total = 0;
+
+    if (this.root === null || this.root === undefined) {
+      return total;
+    }
+
+    while (toVisitStack.length > 0) {
+      const currentNode = toVisitStack.pop();
+      total = total + currentNode.val;
+      for (let child of currentNode.children) {
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    const toVisitStack = [this.root];
+    let total = 0;
 
+    if (this.root === null || this.root === undefined) {
+      return total;
+    }
+
+    while (toVisitStack.length > 0) {
+      const currentNode = toVisitStack.pop();
+      if (currentNode.val % 2 === 0) total += 1;
+      for (let child of currentNode.children) {
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    const toVisitStack = [this.root];
+    let total = 0;
 
+    if (this.root === null || this.root === undefined) {
+      return total;
+    }
+
+    while (toVisitStack.length > 0) {
+      const currentNode = toVisitStack.pop();
+      if (currentNode.val > lowerBound) total += 1;
+      for (let child of currentNode.children) {
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 }
 
